0d222bfbc20ddf2257b33f8b350d5b1a9917abbc
Jason Lee <lwlee2608@gmail.com>
2023-04-15 13:43:55 +0800

    all fields are nullable (#9)

---
+++ proto-java-annotation/src/main/java/io/github/lwlee2608/proto/annotation/exception/UnsupportedTypeException.java
@@ -0,0 +1,7 @@
+package io.github.lwlee2608.proto.annotation.exception;
+
+public class UnsupportedTypeException extends RuntimeException {
+    public UnsupportedTypeException(String message) {
+        super(message);
+    }
+}
