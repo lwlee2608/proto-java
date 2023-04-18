11b0854a738c6cb7cc62d593babb90bd66f1ccd9
jckhoe <ongjackhoe@gmail.com>
2023-04-18 22:17:36 +0800

    Update DownloadMojo to use maven project to resolve and retrieve protoc exe

--- proto-java-plugin/src/main/java/io/github/lwlee2608/proto/plugin/DownloadMojo.java
+++ proto-java-plugin/src/main/java/io/github/lwlee2608/proto/plugin/DownloadMojo.java
@@ -1,45 +1,54 @@
 package io.github.lwlee2608.proto.plugin;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionRequest;
+import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
+import org.apache.maven.artifact.resolver.ResolutionErrorHandler;
+import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
+import org.apache.maven.artifact.versioning.VersionRange;
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.repository.RepositorySystem;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.Os;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.attribute.PosixFilePermission;
+import java.util.List;
 import java.util.Set;
+
+import static io.github.lwlee2608.proto.plugin.PlatformIdentifier.Architecture.AARCH64;
+import static io.github.lwlee2608.proto.plugin.PlatformIdentifier.Architecture.AMD64;
+import static io.github.lwlee2608.proto.plugin.PlatformIdentifier.OperationSystem.LINUX;
+import static io.github.lwlee2608.proto.plugin.PlatformIdentifier.OperationSystem.MAC;
+import static io.github.lwlee2608.proto.plugin.PlatformIdentifier.OperationSystem.WINDOWS;
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.singleton;
 
 @Mojo(name = "download")
 public class DownloadMojo extends AbstractMojo {
 
-    @Parameter(
-            name = "mavenRepoUrl",
-            property = "maven.repo.url",
-            defaultValue = "https://repo1.maven.org/maven2"
-    )
-    // Alternative mirrors: https://repo1.maven.org/maven2, https://repo.maven.apache.org/maven2
-    private String mavenRepoUrl;
-
-    @Parameter(
-            name = "outputDirectory",
-            property = "output.dir",
-            defaultValue = "${project.build.directory}/protoc"
-    )
-    private File outputDirectory;
 
     @Parameter(
             name = "protocVersion",
             property = "protoc.version",
             defaultValue = "3.22.3"
     )
-    private String protocVersion;
+    protected String protocVersion;
+
+    @Parameter(
+            property = "outputDirectory",
+            defaultValue = "${project.build.directory}/protoc/bin",
+            required = true
+    )
+    protected File outputDirectory;
 
     @Parameter(
             name = "os",
@@ -48,84 +57,132 @@ public class DownloadMojo extends AbstractMojo {
     )
     private String os;
 
-    private PlatformIdentifier.OperationSystem operationSystem = PlatformIdentifier.getOperationSystem();
-    private PlatformIdentifier.Architecture architecture = PlatformIdentifier.getArchitecture();
+    @Parameter(
+            required = true,
+            readonly = true,
+            property = "localRepository"
+    )
+    protected ArtifactRepository localRepository;
+
+    @Parameter(
+            required = true,
+            readonly = true,
+            defaultValue = "${project.remoteArtifactRepositories}"
+    )
+    protected List<ArtifactRepository> remoteRepositories;
+
+    @Parameter(defaultValue = "${project}", readonly = true)
+    protected MavenProject project;
+
+    @Parameter(defaultValue = "${session}", readonly = true)
+    protected MavenSession session;
+
+    @SuppressWarnings("deprecation")
+    @Component
+    protected ArtifactFactory artifactFactory;
+
+    @Component
+    protected RepositorySystem repositorySystem;
+
+    @Component
+    protected ResolutionErrorHandler resolutionErrorHandler;
+
+    private final PlatformIdentifier.OperationSystem operationSystem = PlatformIdentifier.getOperationSystem();
+    private final PlatformIdentifier.Architecture architecture = PlatformIdentifier.getArchitecture();
 
     public void execute() {
         if (os == null || os.isEmpty()) {
             os = resolveOperationSystem();
         }
-
-        // Download Binary
-        String downloadUrl = mavenRepoUrl + "/com/google/protobuf/protoc/" + protocVersion + "/protoc-" + protocVersion + "-" + os + ".exe";
-        getLog().info("Starting download protoc binary from " + downloadUrl);
-
-        Path targetProtocBinary = Paths.get(outputDirectory.getAbsolutePath() + "/bin/protoc.exe");
+        final Artifact artifact;
         try {
-            downloadFile(downloadUrl, targetProtocBinary);
-        } catch (Exception e) {
-            getLog().error("Fail to download protoc binary with URL: " + downloadUrl);
-            getLog().error("error: " + e.getMessage());
+            artifact = artifactFactory.createDependencyArtifact(
+                    "com.google.protobuf",
+                    "protoc",
+                    VersionRange.createFromVersionSpec(protocVersion),
+                    "exe",
+                    os,
+                    Artifact.SCOPE_RUNTIME);
+        } catch (InvalidVersionSpecificationException e) {
+            getLog().error("Version " + protocVersion + " not found.");
             throw new RuntimeException(e);
         }
+        // Download artifact
+        resolveBinaryArtifact(artifact);
+    }
 
-        setExecutable(targetProtocBinary);
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    protected void resolveBinaryArtifact(Artifact artifact) {
+        final ArtifactResolutionRequest request = new ArtifactResolutionRequest()
+                .setArtifact(project.getArtifact())
+                .setResolveRoot(false)
+                .setResolveTransitively(false)
+                .setArtifactDependencies(singleton(artifact))
+                .setManagedVersionMap(emptyMap())
+                .setLocalRepository(localRepository)
+                .setRemoteRepositories(remoteRepositories)
+                .setOffline(session.isOffline())
+                .setForceUpdate(session.getRequest().isUpdateSnapshots())
+                .setServers(session.getRequest().getServers())
+                .setMirrors(session.getRequest().getMirrors())
+                .setProxies(session.getRequest().getProxies());
 
-        getLog().info("Proto Java plugin completed");
+        final ArtifactResolutionResult result = repositorySystem.resolve(request);
+
+        try {
+            resolutionErrorHandler.throwErrors(request, result);
+        } catch (ArtifactResolutionException e) {
+            throw new RuntimeException("Unable to resolve artifact: " + e.getMessage(), e);
+        }
+
+        Set<Artifact> artifacts = result.getArtifacts();
+
+        if (artifacts == null || artifacts.isEmpty()) {
+            throw new RuntimeException("Unable to resolve artifact");
+        }
+
+        Artifact resolvedBinaryArtifact = artifacts.iterator().next();
+        File sourceFile = resolvedBinaryArtifact.getFile();
+        String targetFileName = "protoc.exe";
+        File targetFile = new File(outputDirectory, targetFileName);
+
+        if (targetFile.exists()) {
+            return;
+        }
+
+        try {
+            FileUtils.forceMkdir(outputDirectory);
+            FileUtils.copyFile(sourceFile, targetFile);
+            if (!Os.isFamily(Os.FAMILY_WINDOWS)) {
+                targetFile.setExecutable(true);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to create or copy file: " + e.getMessage(), e);
+        }
     }
 
     private String resolveOperationSystem() {
-        // follow naming convention in https://repo1.maven.org/maven2/com/google/protobuf/protoc/3.22.3/
         switch (operationSystem) {
             case WINDOWS:
                 return "windows-x86_64";
             case LINUX:
                 switch (architecture) {
-                    case AMD64: return "linux-x86_64";
-                    case AARCH64: return "osx-aarch_64";
+                    case AMD64:
+                        return "linux-x86_64";
+                    case AARCH64:
+                        return "osx-aarch_64";
                 }
                 break;
             case MAC:
                 switch (architecture) {
-                    case AMD64: return "osx-x86_64";
-                    case AARCH64: return "osx-aarch_64";
+                    case AMD64:
+                        return "osx-x86_64";
+                    case AARCH64:
+                        return "osx-aarch_64";
                 }
                 break;
         }
-
+        getLog().error("Unsupported OS");
         throw new RuntimeException("Unsupported OS or architecture. Please specific os.identifier parameter for proto-java-plugin");
-    }
-
-    private void downloadFile(String url, Path outputFile) throws Exception {
-        URL downloadUrl = new URL(url);
-        ReadableByteChannel rbc = Channels.newChannel(downloadUrl.openStream());
-
-        // Create directory structure for output file
-        Path outputDirectory = outputFile.getParent();
-        if (outputDirectory != null && !Files.exists(outputDirectory)) {
-            Files.createDirectories(outputDirectory);
-        }
-
-        FileOutputStream fos = new FileOutputStream(outputFile.toFile());
-        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-        fos.close();
-        rbc.close();
-    }
-
-    private void setExecutable(Path path) {
-        // Make binary executable
-        if (operationSystem == PlatformIdentifier.OperationSystem.WINDOWS) {
-            return;
-        }
-
-        try {
-            Set<PosixFilePermission> filePermissions = Files.getPosixFilePermissions(path);
-            filePermissions.add(PosixFilePermission.OWNER_EXECUTE);
-
-            Files.setPosixFilePermissions(path, filePermissions);
-        } catch (IOException e) {
-            getLog().error("Fail to make binary executable");
-            throw new RuntimeException(e);
-        }
     }
 }
